f1e797f9f9ebc86676a9e7bb20d68b82ba724086
Benedict <benedictnnaoma0@gmail.com>
2025-10-16 13:39:46 +0100

    listing page fix

--- listup_frontend/src/app/listings/page.tsx
+++ listup_frontend/src/app/listings/page.tsx
@@ -471,7 +471,7 @@ function ListingsPageContent() {
 
         {/* Listings Grid */}
         <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {filteredListings.map((listing) => (
+          {filteredListings?.map((listing) => (
             <ListingCard key={listing.id} listing={listing} />
           ))}
         </div>
