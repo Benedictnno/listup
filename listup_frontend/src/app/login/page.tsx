992b9c98230a39a06c5853200d96ffdf94f2463a
Benedictnno <benedictnnaoma0@gmail.com>
2026-04-30 10:28:54 +0100

    debug: signup routing

--- listup_frontend/src/app/login/page.tsx
+++ listup_frontend/src/app/login/page.tsx
@@ -237,7 +237,7 @@ export default function LoginPage() {
         <GoogleSignInButton
           onSuccess={({ isNewUser }) => {
             if (isNewUser) setShowVendorPrompt(true);
-            else router.push('/dashboard');
+            else router.push('/listings');
           }}
         />
         
